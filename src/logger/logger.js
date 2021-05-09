6e79bb97988fff1700d7beb38b0b292b64b1a4d7
Elia <reutlinger.elia@gmail.com>
2021-05-09 19:19:15 +0200

    Update logger.js

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -75,7 +75,7 @@ exports.start = async function start(dcClient, repeat, logs) {
                 await channel.send(new Discord.MessageEmbed({
                     color: line.color,
                     footer: {
-                        text: line.type.toUpperCase()
+                        text: line.type.toUpperCase() + ': ' + line.date
                     },
                     description: line.content
                 }))
